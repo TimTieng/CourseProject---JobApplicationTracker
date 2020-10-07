5369e8f36e911f754500c5db24f91fb8a7fed03d
Tim Tieng <51296279+TimTieng@users.noreply.github.com>
2020-10-07 12:04:34 -0400

    Add files via upload

---
+++ Migrations/20200910155145_JobApplicationCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TiengInClassWebApp.Migrations
+{
+    public partial class JobApplicationCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    jobRequisitionNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    jobTitle = table.Column<string>(nullable: false),
+                    hiringCompany = table.Column<string>(nullable: false),
+                    jobLocation = table.Column<string>(nullable: false),
+                    applicationSubmissionDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.jobRequisitionNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
---
+++ Migrations/20200917151618_ApplicationNotes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TiengInClassWebApp.Migrations
+{
+    public partial class ApplicationNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "applicationNotes",
+                table: "Jobs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "applicationNotes",
+                table: "Jobs");
+        }
+    }
+}
